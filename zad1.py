4242ed05c964029ae6c9ba28ea3f8908f54d5401
ks440007 <ks440007@students.mimuw.edu.pl>
2025-03-21 07:40:54 +0100

    opis funkcji find_centroid

--- zad1.py
+++ zad1.py
@@ -70,12 +70,16 @@ class RegBinTree:
         self.root.calc_node()
         pass
 
+    # Funkcja znajdująca centroid w drzewie.
     def find_centroid(self) -> RegBinNode|Leaf|None:
-        # TODO: uzupełnij tę metodę (możesz deklarować dodatkowe metody tutaj, w PosBinNode lub globalnie).
-        # Możesz założyć, że pola 'size' w wierzchołkach zawierają prawidłowe wartości.
+        # Funkcja zwraca None jeśli drzewo jest puste.
         if self.root is None:
             return None
+        # Funkcja wylicza maksymalną możliwą wielkość każdego z poddrzew powstałych po usunięciu centroidu.
         max_size = int(self.root.size / 2)
+        # Rekurencyjna funkcja pomocnicza. Dla wskazanego węzła sprawdza, czy któreś z jego poddrzew (o ile istnieją) jest rozmiaru większego niż max_size.
+        # Jeśli tak (może być takie co najwyżej jedno), to oznacza, że centroid musi znajdywać się w tym poddrzewie, zatem rekurencja zwraca wynik po wywołaniu
+        # siebie dla tego poddrzewa. Jeśli nie ma poddrzewa o takim rozmiarze, to rekurencja zwraca wskazany węzeł jako szukany centroid.
         def find_node(node: RegBinNode|Leaf) -> RegBinNode|Leaf:
             if type(node) is RegBinNode:
                 if type(node.left) is not None and node.left.size > max_size:
@@ -83,6 +87,10 @@ class RegBinTree:
                 if type(node.right) is not None and node.right.size > max_size:
                     return find_node(node.right)
             return node
+        # Funkcja zwraca wynik po wywołaniu rekurencji pomocniczej na korzeniu. Ponieważ rekurencja zaczyna się od korzenia, to jeśli żadne z poddrzew
+        # nie będzie większe od max_size, korzeń będzie centroidem. W przeciwnym wypadku rekurencja wywoła się od poddrzewa, w którym centroid
+        # musi się znajdować, zatem rekurencja zawsze znajdzie centroid drzewa.
+        # Rekurencja może wywoływać siebie jedynie w dół drzewa, a więc nie zostanie wywołana więcej razy, niż wynosi jego wysokość.
         return find_node(self.root)
 
     def __str__(self):
